799086a1d23777abed1b17356a95993ac01157a0
VasylEpov <vepov86@gmail.com>
2023-11-09 22:43:37 +0200

    addMarkup

---
+++ src/js/gallery.js
@@ -0,0 +1,35 @@
+export { renderGallery };
+
+const gallery = document.querySelector('.gallery');
+
+function renderGallery(images) {
+  const markup = images
+    .map(image => {
+      const {
+        id,
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = image;
+      return `
+        <a class="gallery__link" href="${largeImageURL}">
+          <div class="gallery-item" id="${id}">
+            <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item"><b>Likes</b>${likes}</p>
+              <p class="info-item"><b>Views</b>${views}</p>
+              <p class="info-item"><b>Comments</b>${comments}</p>
+              <p class="info-item"><b>Downloads</b>${downloads}</p>
+            </div>
+          </div>
+        </a>
+      `;
+    })
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
