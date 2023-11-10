281de5cd31f9a7e7a490cd6cc7988a31e7e23f5e
GLiliya19 <liliyagizhitska@icloud.com>
2023-11-11 01:23:09 +0200

    support section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import './js/modal'
-import './js/header'
-import './js/scroll'
-import './js/footer'
-import './js/book-api'
+import './js/modal';
+import './js/header';
+import './js/scroll';
+import './js/footer';
+import './js/book-api';
+import './js/support';
