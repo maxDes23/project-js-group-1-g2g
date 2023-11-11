1f388de9a2375bdf467ffbe7bf6c33b512f323a7
Pavlo-Piddunyi <135760789+Pavlo-Piddunyi@users.noreply.github.com>
2023-11-11 14:07:25 +0200

    scroll up (good)

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,13 +1,13 @@
 (() => {
   window.addEventListener('scroll', function () {
-    var button = document.getElementById('back-to-up');
+    var button = document.getElementById('scroll-up');
     if (button) {
       button.style.visibility =
         window.scrollY > window.innerHeight ? 'visible' : 'hidden';
     }
   });
 
-  document.querySelector('#back-to-up').addEventListener('click', scrollToTop);
+  document.querySelector('#scroll-up').addEventListener('click', scrollToTop);
 
   function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
