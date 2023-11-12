7891f4feacbfc39bcae5f9577849a002c8ec00c6
sunny7657 <ksvetlana466@gmail.com>
2023-11-12 12:15:38 +0100

    book-api fixed queries

--- src/js/book-api.js
+++ src/js/book-api.js
@@ -2,30 +2,24 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://books-backend.p.goit.global/books/';
 
-const settings = {
-  headers: {
-    'Content-Type': 'application/json',
-  },
-};
-
 // Перелік категорій книг:
 async function serviceAllCategory() {
-  return await axios.get('category-list', settings);
+  return await axios.get('category-list');
 }
 
 // Популярні книги, що належать до усіх категорій:
 async function serviceTopBooks() {
-  return await axios.get('top-books', settings);
+  return await axios.get('top-books');
 }
 
 // Книги окремої категорії:
 async function serviceSelectedCategory(selectedCategory) {
-  return await axios.get(`category?category=${selectedCategory}`, settings);
+  return await axios.get(`category?category=${selectedCategory}`);
 }
 
 // Детальна інформація про книгу:
 async function serviceSelectedBook(bookId) {
-  return await axios.get(`_id=${bookId}`, settings);
+  return await axios.get(`${bookId}`);
 }
 
 export {
