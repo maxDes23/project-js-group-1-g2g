627b3dddac563e6e8cb1bcbb0451ab0d1d319b48
GLiliya19 <liliyagizhitska@icloud.com>
2023-11-13 22:20:28 +0200

    Update support.js

--- src/js/support.js
+++ src/js/support.js
@@ -54,7 +54,6 @@ supOrgs.sort((a, b) =>
 );
 
 const supportList = document.querySelector('.splide__list');
-console.log(supportList);
 
 supOrgs.forEach(supOrg => {
   const li = `<li class="splide__slide">
