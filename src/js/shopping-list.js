d1d01aa43a25b05571f128da99d3f7fc5262fc3a
MadDog83 <130376768+MadDog83@users.noreply.github.com>
2023-11-14 00:53:53 +0100

    add/and fix/shoppinglist js

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -1,91 +1,114 @@
+const SHOPPING_LIST_STORAGE_KEY = 'storage-of-books';
+const shoppingList = JSON.parse(localStorage.getItem(SHOPPING_LIST_STORAGE_KEY)) || [];
 
-// 1. Create a function to fetch the list of books from localStorage.
-function getBooks() {
-    const books = JSON.parse(localStorage.getItem('books')) || [];
-    return books;
+const divEl = document.querySelector('.shop_title_list');
+const amazonIconPath = './img/modal/amazon.png';
+const bookShopIconPath = './img/shopping/book.png';
+const svgTrashIcon = './img/icons.svg';
+
+
+const pageSize = 3;
+let totalPages = Math.ceil(shoppingList.length / pageSize);
+let currentPage = 1;
+let startIndex = (currentPage - 1) * pageSize;
+let endIndex = startIndex + pageSize;
+let itemsOnPage = shoppingList.slice(startIndex, endIndex);
+
+function renderMarkUp(itemsOnPage) {
+    return itemsOnPage
+        .map(
+            ({
+                _id,
+                title,
+                author,
+                description,
+                list_name,
+                book_image,
+                amazon_product_url,
+                buy_links: [bookshop],
+            }) => {
+                return `
+                <article class="shop_card">
+                    <div class="card-img">
+                        <img class="shop__card-img" src="${book_image}" alt="${title}" />
+                    </div>
+
+                    <div class="card-title">
+                        <h3 class="shop__book-title">${title}</h3>
+                        <p class="shop__book-category">${list_name}</p>
+                    </div>
+
+                    <div class="card-description">
+                        <p class="shop__book-description">${description}</p>
+                    </div>
+
+                    <div class="card-author">
+                        <p class="shop__book-author">${author}</p>
+                    </div>
+
+                    <div class="card-shoplist">
+                        <ul class="shop__book-shoplist">
+                            <li class="store">
+                                <a href="${amazon_product_url}" target="_blank" rel="noopener noreferrer nofollow" aria-label="Amazon link">
+                                    <img class="modal-shop-img shopping-shopimg amazon" src="${amazonIconPath}" alt="Amazon link" alt="Amazon live page"/>
+                                </a>
+                            </li>
+                            <li class="store">
+                                <a href="${bookshop.url}" target="_blank" rel="noopener noreferrer nofollow" aria-label="BookShop link">
+                                    <img class="modal-shop-img shopping-shopimg book-shop" src="${bookShopIconPath}" alt="BookShop link" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <button class="shop_card-btn" type="button" data-book-id="${_id}" aria-label="Remove book from shopping list">
+                        <svg class="trash-icon" data-book-id="${_id}" width="17" height="17">
+                            <use href="${svgTrashIcon}#trash-icon"></use>
+                        </svg>
+                    </button>
+                </article>
+                `;
+            }
+        )
+        .join('');
 }
 
-// 2. Create a function to render the list of books on the page.
-function renderBooks() {
-    const books = getBooks();
-    const bookList = document.querySelector('.shop_title_list');
-    if (!bookList) {
+function isEmpty() {
+    if (!shoppingList.length) {
+        divEl.innerHTML = `<div class=""><p class="no__info">This page is empty, add some books and proceed to order.</p><img class="no__picture" src="./img/shopping/books-desk.png" alt="Shop is Empty"></div >`;
         return;
     }
-    if (books.length === 0) {
-        bookList.innerHTML = `<p class="no__info">This page is empty, add some books and proceed to order.</p>
-        <img class="no__picture" src="../img/shopping/books-desk.png" alt="Shop is Empty" >`;
-        return;
+    divEl.insertAdjacentHTML('beforeend', renderMarkUp(itemsOnPage));
+}
+
+isEmpty();
+
+divEl.addEventListener('click', event => {
+    if (event.target.closest('.shop_card-btn')) {
+        const BookID = event.target.getAttribute('data-book-id');
+
+        const bookIndex = shoppingList.findIndex(
+            bookInStorage => bookInStorage._id === BookID
+        );
+
+        shoppingList.splice(bookIndex, 1);
+
+        localStorage.setItem(
+            SHOPPING_LIST_STORAGE_KEY,
+            JSON.stringify(shoppingList)
+        );
+
+        if (!shoppingList.length) {
+            divEl.innerHTML = `<div class=""><p class="no__info">This page is empty, add some books and proceed to order.</p><img class="no__picture" src="./img/shopping/books-desk.png" alt="Shop is Empty"></div >`;
+            return;
+        } else if (!sliceArrayBooks().length) {
+            previousButton.click();
+            destroyChildElement(paginationContainerPages);
+        } else {
+            divEl.innerHTML = renderMarkUp(sliceArrayBooks());
+            console.log(sliceArrayBooks());
+            destroyChildElement(paginationContainerPages);
+        }
+        checkingArrayBooks();
     }
-    bookList.innerHTML = '';
-    books.forEach((book) => {
-        const bookCard = createBookCard(book);
-        bookList.appendChild(bookCard);
-    });
-}
-
-// 3. Create a function to render a single book card.
-function createBookCard(book) {
-    const bookCard = document.createElement('div');
-    bookCard.classList.add('book-card');
-
-    const bookImage = document.createElement('img');
-    bookImage.classList.add('book-image');
-    bookImage.src = book.image;
-    bookImage.alt = book.title;
-    bookCard.appendChild(bookImage);
-
-    const bookTitle = document.createElement('h3');
-    bookTitle.classList.add('book-title');
-    bookTitle.textContent = book.title;
-    bookCard.appendChild(bookTitle);
-
-    const bookCategory = document.createElement('p');
-    bookCategory.classList.add('book-category');
-    bookCategory.textContent = book.category;
-    bookCard.appendChild(bookCategory);
-
-    const bookDescription = document.createElement('p');
-    bookDescription.classList.add('book-description');
-    bookDescription.textContent = book.description;
-    bookCard.appendChild(bookDescription);
-
-    const bookAuthor = document.createElement('p');
-    bookAuthor.classList.add('book-author');
-    bookAuthor.textContent = `by ${book.author}`;
-    bookCard.appendChild(bookAuthor);
-
-    const bookLinks = document.createElement('div');
-    bookLinks.classList.add('book-links');
-    book.links.forEach((link) => {
-        const linkElement = document.createElement('a');
-        linkElement.href = link.url;
-        linkElement.textContent = link.name;
-        bookLinks.appendChild(linkElement);
-    });
-    bookCard.appendChild(bookLinks);
-
-    const deleteButton = document.createElement('button');
-    deleteButton.classList.add('delete-button');
-    deleteButton.textContent = 'Remove from list';
-    deleteButton.addEventListener('click', () => {
-        deleteBook(book.id);
-    });
-    bookCard.appendChild(deleteButton);
-
-    return bookCard;
-}
-
-// 4. Create a function to delete a book from the list and from localStorage.
-function deleteBook(bookId) {
-    let books = getBooks();
-    books = books.filter((book) => book.id !== bookId);
-    localStorage.setItem('books', JSON.stringify(books));
-    renderBooks();
-}
-
-// 5. Call the functions to fetch and render the list of books when the page loads.
-window.addEventListener('load', () => {
-    renderBooks();
 });
 
