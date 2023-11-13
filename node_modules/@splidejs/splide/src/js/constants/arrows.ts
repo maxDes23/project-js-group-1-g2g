2023726799e23a53f48711751bd191cea89b88ba
pavloit <dba.devop@gmail.com>
2023-11-13 15:45:03 +0200

    Revert "remove .gitignore content"
    This reverts commit 79e54e877000fd280fc13d75b3c25aec78c16bc0.

--- node_modules/@splidejs/splide/src/js/constants/arrows.ts
+++
@@ -1,5 +0,0 @@
-const ARROW = 'Arrow';
-export const ARROW_LEFT  = `${ ARROW }Left`;
-export const ARROW_RIGHT = `${ ARROW }Right`;
-export const ARROW_UP    = `${ ARROW }Up`;
-export const ARROW_DOWN  = `${ ARROW }Down`;
