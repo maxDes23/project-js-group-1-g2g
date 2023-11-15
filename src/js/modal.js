4f5c53fd6e33014d493586ca21903d09c12faf3d
Sychov-Yurii <sychov.yurii@outlook.com>
2023-11-15 07:03:52 +0100

    css

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,29 +4,35 @@ import { serviceSelectedBook } from './book-api';
 
 async function showBookInfo(bookInfo) {
   try {
-    const { book_image, title, author, list_name, amazon_product_url } =
+    const { book_image, title, author, list_name, amazon_product_url, buy_links: [bookshop] } =
       bookInfo;
 
-    //   console.log('book Image: ', book_image,);
-    //   console.log('book title: ', title,);
-    //   console.log('book author: ', author,);
-    //   console.log('list Name: ', list_name,);
-    //   console.log('Amazon Url: ', amazon_product_url,);
 
     const modal = document.querySelector('.modal');
     //   console.log('modal Constant', modal);
 
+    
     modal.innerHTML = '';
 
     // разметка
 
     const elements = [
-      `<button class="modal-close-button">Close</button>`,
+      `<button class="modal-close-button">
+      <svg
+    class="modal__cross js-modal-close"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+  >
+    <path
+      d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"
+    />
+  </svg></button>`,
       `<img src="${book_image}" class="modal-image">`,
       `<h2 class="modal__title">${title}</h2>`,
       `<p class="modal-title-name">Author: ${author}</p>`,
       `<p class="modal-title-text">List Name: ${list_name}</p>`,
       `<a href="${amazon_product_url}" class="modal-link">Amazon Link</a>`,
+      `<a href="${bookshop}" class="modal-link">Book Link</a>`,
       `<button class="modal-button-add">ADD</button>`,
     ];
 
@@ -36,14 +42,18 @@ async function showBookInfo(bookInfo) {
     // Add event listener to the close button
     const closeButton = document.querySelector('.modal-close-button');
     closeButton.addEventListener('click', () => {
+
+      const modal = document.querySelector('.modal');
       modal.classList.remove('active');
     });
-
+  
     modal.classList.add('active');
+
   } catch (error) {
     console.log(error);
   }
 }
+
 
 function connectModal() {
   const modalGallery = document.querySelector('.category-container');
@@ -61,6 +71,7 @@ async function onBookClick(event) {
 
   const bookData = await serviceSelectedBook(bookId);
   // console.log(bookData.data);
+  
   showBookInfo(bookData.data);
 }
 
