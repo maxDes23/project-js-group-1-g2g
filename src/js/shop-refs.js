e2313fd9a8155e75d239d4a3be8798a2b5027706
pavloit <dba.devop@gmail.com>
2023-11-16 12:57:39 +0200

    fix basket in progress

--- src/js/shop-refs.js
+++ src/js/shop-refs.js
@@ -1,7 +1,7 @@
 export default function () {
     return {
         // Shopping list
-    divEl: document.querySelector('.blank-basket'),
+    divEl: document.querySelector('.filled-basket'),
     addBtnEL: document.querySelector('.shop_card-btn'),
        // Paginations
     }
