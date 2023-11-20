e1285578533a2211760c82b7c08408ed0cb6bfb5
MadDog83 <130376768+MadDog83@users.noreply.github.com>
2023-11-20 23:54:26 +0100

    add pagination

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -1,35 +1,30 @@
-import amazonImg from "../img/shopping/amazon.png"
+
 import icons from "../img/icons.svg"
 import getRefs from './shop-refs.js';
 import getShopIconsPaths from './shop-icons-path.js';
-// import { storedBooksArray } from "./modal.js";
 
 
 const SHOPPING_LIST_STORAGE_KEY = 'books';
 let storedBooksArray = JSON.parse(localStorage.getItem(SHOPPING_LIST_STORAGE_KEY)) || []
 const blankBasket = document.querySelector('.blank-basket')
 
-const { divEl, 
-    previousButton, 
-    paginationPagesStart,  
-  } = getRefs(); {
+// Getting references to DOM elements
+const { divEl } = getRefs();
 
-}
-const {
-    bookShopIconPath,
-    amazonIconPath,
-    // svgTrashIcon,
-  } = getShopIconsPaths();
+// Getting paths for shop icons
+const { bookShopIconPath, amazonIconPath } = getShopIconsPaths();
 
-const pageSize = 6;
-let currentPage = 1;
+// Pagination variables
+const pageSize = 3;
+let currentPage = parseInt(localStorage.getItem('currentPage')) || 1;
 let startIndex = (currentPage - 1) * pageSize;
 let endIndex = startIndex + pageSize;
 let itemsOnPage = storedBooksArray.slice(startIndex, endIndex);
 
+// Function to render the markup for the shopping list
 function renderMarkUp(storageArr) {
-
-    divEl.innerHTML = storageArr.map(
+    divEl.innerHTML = storageArr
+        .map(
             ({
                 id,
                 title,
@@ -37,117 +32,192 @@ function renderMarkUp(storageArr) {
                 description,
                 list_name,
                 book_image,
-                amazon_product_url
+                amazon_product_url,
             }) => {
                 return `
-                <article class="shop_card">
-                    <div class="card-img">
-                        <img class="shop__card-img" src="${book_image}" alt="${title}" />
-                    </div>
+                    <article class="shop_card">
+                        <div class="card-img">
+                            <img class="shop__card-img" src="${book_image}" alt="${title}" />
+                        </div>
 
-                    <div class="card-title">
-                        <h3 class="shop__book-title">${title}</h3>
-                        <p class="shop__book-category">${list_name}</p>
-                    </div>
+                        <div class="card-title">
+                            <h3 class="shop__book-title">${title}</h3>
+                            <p class="shop__book-category">${list_name}</p>
+                        </div>
 
-                    <div class="card-description">
-                        <p class="shop__book-description">${description}</p>
-                    </div>
+                        <div class="card-description">
+                            <p class="shop__book-description">${description}</p>
+                        </div>
 
-                    <div class="card-author">
-                        <p class="shop__book-author">${author}</p>
-                    </div>
+                        <div class="card-author">
+                            <p class="shop__book-author">${author}</p>
+                        </div>
 
-                    <div class="card-shoplist">
-                        <ul class="shop__book-shoplist">
-                            <li class="store">
-                                <a href="${amazon_product_url}" target="_blank" rel="noopener noreferrer nofollow" aria-label="Amazon link">
-                                    <img class="amazon" src="${amazonImg}" alt="Amazon link" alt="Amazon live page"/>
-                                </a>
-                            </li>
-                            <li class="store">
-                                <a href="${amazon_product_url}" target="_blank" rel="noopener noreferrer nofollow" aria-label="BookShop link">
-                                    <img class="book-shop" src="${bookShopIconPath}" alt="BookShop link" />
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <button class="delete-btn" type="button" data-book-id="${id}" aria-label="Remove book from shopping list">
-                        <svg class="trash-icon" data-book-id="${id}" width="18" height="18">
-                            <use class="t-ico" data-book-id="${id}" href="${icons}#trash-icon"></use>
-                        </svg>
-                    </button>
-                </article>
+                        <div class="card-shoplist">
+                            <ul class="shop__book-shoplist">
+                                <li class="store">
+                                    <a href="${amazon_product_url}" target="_blank" rel="noopener noreferrer nofollow" aria-label="Amazon link">
+                                        <img class="amazon" src="${amazonIconPath}" alt="Amazon link" alt="Amazon live page"/>
+                                    </a>
+                                </li>
+                                <li class="store">
+                                    <a href="https://www.bookshop.org" target="_blank" rel="noopener noreferrer nofollow" aria-label="BookShop link">
+                                        <img class="book-shop" src="${bookShopIconPath}" alt="BookShop link" />
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <button class="delete-btn" type="button" data-book-id="${id}" aria-label="Remove book from shopping list">
+                            <svg class="trash-icon" data-book-id="${id}" width="18" height="18">
+                                <use class="t-ico" data-book-id="${id}" href="${icons}#trash-icon"></use>
+                            </svg>
+                        </button>
+                    </article>
                 `;
             }
         )
         .join('');
 }
 
+// Function to check if the shopping list is empty
 function isEmpty() {
     if (!storedBooksArray.length) {
-        divEl.innerHTML = ''
-        return blankBasket.classList.remove('display-none');
+        divEl.innerHTML = '';
+        blankBasket.classList.remove('display-none');
+    } else {
+        blankBasket.classList.add('display-none');
+        updateItemsOnPage();
     }
-   return renderMarkUp(storedBooksArray)
 }
 
+// Initial check for empty shopping list
 isEmpty();
 
-// function sliceArrayBooks() {
-//     startIndex = (currentPage - 1) * pageSize;
-//     endIndex = startIndex + pageSize;
-//     itemsOnPage = storedBooksArray.slice(startIndex, endIndex);
-//     return itemsOnPage;
-// }
-
-// function destroyChildElement(parent) {
-//     if (parent) {
-//         while (parent.firstChild) {
-//             parent.removeChild(parent.firstChild);
-//         }
-//     }
-// }
-
-divEl.addEventListener('click', onDivElClick)
-
+// Event handler for click events on the shopping list
 function onDivElClick(event) {
-    
-    if (event.target.closest('.delete-btn')
-        || event.target.closest('.trash-icon')
-        || event.target.closest('.t-ico') ) {
-        storedBooksArray = storedBooksArray.filter(book => book.id != event.target.getAttribute('data-book-id'))
+    if (
+        event.target.closest('.delete-btn') ||
+        event.target.closest('.trash-icon') ||
+        event.target.closest('.t-ico')
+    ) {
+        // Remove the book from the shopping list
+        storedBooksArray = storedBooksArray.filter(
+            (book) => book.id != event.target.getAttribute('data-book-id')
+        );
+        // Update the local storage
         localStorage.setItem('books', JSON.stringify(storedBooksArray));
+        // Check if the shopping list is empty
+        isEmpty();
+        // Check if the current page is greater than the total pages
+        const totalPages = Math.ceil(storedBooksArray.length / pageSize);
+        if (currentPage > totalPages) {
+            currentPage = totalPages;
+            localStorage.setItem('currentPage', currentPage);
+        }
+
+        updateItemsOnPage();
+
+        updatePagination();
     }
-    isEmpty()
 }
 
+// Add event listener to the shopping list container
+divEl.addEventListener('click', onDivElClick);
 
-//         if (!storedBooksArray.length) {
-//             divEl.innerHTML = ``;
-//             return isEmpty();
-//         } else if (!sliceArrayBooks().length) {
-//             previousButton.click();
-//             destroyChildElement(paginationPagesStart);
-//         } else {
-//             divEl.innerHTML = renderMarkUp(sliceArrayBooks());
-//             destroyChildElement(paginationPagesStart);
-//         }
+// Function to update the items displayed on the current page
+function updateItemsOnPage() {
+    startIndex = (currentPage - 1) * pageSize;
+    endIndex = startIndex + pageSize;
+    itemsOnPage = storedBooksArray.slice(startIndex, endIndex);
+    renderMarkUp(itemsOnPage);
+}
 
+// P A G I N A T I O N
 
+const startButton = document.querySelector('[name="startButton"]');
+const previousButton = document.querySelector('[name="previousButton"]');
+const nextButton = document.querySelector('[name="nextButton"]');
+const endButton = document.querySelector('[name="endButton"]');
+const paginationPagesStart = document.querySelector('.pagination_pages_start');
+const paginationArrow = document.querySelector('.pagination_arrow');
 
-// Add a function to check if the book is already in the shopping list
-// function isBookInShoppingList(bookId) {
-//     return storedBooksArray.some(book => book.id === bookId);
-// }
+// Function to jump to the last page
+function jumpToLastPage() {
+    currentPage = Math.ceil(storedBooksArray.length / pageSize);
+    updateItemsOnPage();
+    updatePagination();
+}
 
-// Add an event listener to the document to listen for the 'add-to-cart' event
-// document.addEventListener('add-to-cart', event => {
-//     const book = event.detail.book;
-//     if (!isBookInShoppingList(book.id)) {
-//         storedBooksArray.push(book);
-//         localStorage.setItem(SHOPPING_LIST_STORAGE_KEY, JSON.stringify(storedBooksArray));
-//     }
-// });
+// Function to jump to the first page
+function jumpToFirstPage() {
+    currentPage = 1;
+    updateItemsOnPage();
+    updatePagination();
+}
+
+// Function to go to the previous page
+function goToPreviousPage() {
+    if (currentPage > 1) {
+        currentPage--;
+        updateItemsOnPage();
+        updatePagination();
+    }
+}
+
+// Function to go to the next page
+function goToNextPage() {
+    const totalPages = Math.ceil(storedBooksArray.length / pageSize);
+    if (currentPage < totalPages) {
+        currentPage++;
+        updateItemsOnPage();
+        updatePagination();
+    }
+}
+
+function updatePagination() {
+    const totalPages = Math.ceil(storedBooksArray.length / pageSize);
+    let paginationHTML = '';
+
+    for (let i = 1; i <= totalPages; i++) {
+        paginationHTML += `<button class="pagination-button" data-page="${i}">${i}</button>`;
+    }
+
+    paginationPagesStart.innerHTML = paginationHTML;
+
+    const paginationButtons = document.querySelectorAll('.pagination-button');
+    paginationButtons.forEach((button) => {
+        button.addEventListener('click', function () {
+            currentPage = parseInt(this.dataset.page);
+            updateItemsOnPage();
+            updatePagination();
+        });
+
+        // Add class 'active' to the current page button
+        if (button.dataset.page == currentPage) {
+            button.classList.add('active');
+        } else {
+            button.classList.remove('active');
+        }
+    });
+
+    if (storedBooksArray.length > pageSize) {
+        paginationArrow.style.display = 'flex';
+    } else {
+        paginationArrow.style.display = 'none';
+    }
+}
+
+// Add event listeners to the pagination buttons
+startButton.addEventListener('click', jumpToFirstPage);
+previousButton.addEventListener('click', goToPreviousPage);
+nextButton.addEventListener('click', goToNextPage);
+endButton.addEventListener('click', jumpToLastPage);
+
+updatePagination();
+
+// Save the current page to localStorage before the page is unloaded
+window.addEventListener('beforeunload', function () {
+    localStorage.setItem('currentPage', currentPage);
+});
 
 
